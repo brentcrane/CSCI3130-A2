aabffd9291ed56918e52ad22063b090fbee8d82f
brentcrane <35691760+brentcrane@users.noreply.github.com>
2018-02-06 11:48:44 -0400

    My initial commit of my software engineering assignment 2 files.

---
+++ app/src/main/java/com/dal/csci3130/passwordvalidator/Validator.java
@@ -0,0 +1,13 @@
+package com.dal.csci3130.passwordvalidator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Validator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validator);
+    }
+}
